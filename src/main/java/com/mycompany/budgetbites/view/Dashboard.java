02e77442b58f0400791802321d24d8ee3c25aee7
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-01-06 19:43:32 +0545

    UI fix

--- src/main/java/com/mycompany/budgetbites/view/Dashboard.java
+++ src/main/java/com/mycompany/budgetbites/view/Dashboard.java
@@ -200,7 +200,7 @@ public class Dashboard extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 850, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
