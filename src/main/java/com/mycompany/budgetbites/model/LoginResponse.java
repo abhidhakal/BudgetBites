039a2ec2bef923ea3087df2d1a01f9942ae6c4bc
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-01-23 12:22:37 +0545

    added login response class

---
+++ src/main/java/com/mycompany/budgetbites/model/LoginResponse.java
@@ -0,0 +1,15 @@
+public class LoginResponse {
+    private String email;
+    private String password;
+    public LoginResponse(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPassword(){
+        return this.password;
+    }  
+}
