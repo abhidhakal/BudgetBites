1c3884fd5d7565bf6e944d1e247ee2138a39e860
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-01-30 11:21:21 +0545

    backend and database connection

--- src/main/java/com/mycompany/budgetbites/view/ForgotPassword.java
+++ src/main/java/com/mycompany/budgetbites/view/ForgotPassword.java
@@ -4,6 +4,10 @@
  */
 package com.mycompany.budgetbites.view;
 
+import com.mycompany.budgetbites.database.MySqlConnection;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mohandhakal
@@ -32,15 +36,14 @@ public class ForgotPassword extends javax.swing.JFrame {
         newPwLabel = new javax.swing.JLabel();
         confirmPwLabel = new javax.swing.JLabel();
         emailField1 = new javax.swing.JTextField();
-        oldPwField1 = new javax.swing.JPasswordField();
         newPwField1 = new javax.swing.JPasswordField();
         confirmPwField1 = new javax.swing.JPasswordField();
         clearBtn = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         newPwCheckbox = new javax.swing.JCheckBox();
         confirmPwCheckbox = new javax.swing.JCheckBox();
-        oldPwCheckbox = new javax.swing.JCheckBox();
         jLabel1 = new javax.swing.JLabel();
+        tokenField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -54,7 +57,7 @@ public class ForgotPassword extends javax.swing.JFrame {
 
         oldPwLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         oldPwLabel.setForeground(new java.awt.Color(90, 0, 0));
-        oldPwLabel.setText("Old Password:");
+        oldPwLabel.setText("Token:");
 
         newPwLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         newPwLabel.setForeground(new java.awt.Color(90, 0, 0));
@@ -67,22 +70,10 @@ public class ForgotPassword extends javax.swing.JFrame {
         emailField1.setBackground(new java.awt.Color(227, 235, 219));
         emailField1.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
         emailField1.setForeground(new java.awt.Color(102, 102, 102));
-        emailField1.setText("johndoe@gmail.com");
         emailField1.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
         emailField1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 emailField1MouseClicked(evt);
-            }
-        });
-
-        oldPwField1.setBackground(new java.awt.Color(227, 235, 219));
-        oldPwField1.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
-        oldPwField1.setForeground(new java.awt.Color(102, 102, 102));
-        oldPwField1.setText("x27M4/q]x\"e6");
-        oldPwField1.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-        oldPwField1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                oldPwField1MouseClicked(evt);
             }
         });
 
@@ -110,7 +101,7 @@ public class ForgotPassword extends javax.swing.JFrame {
 
         clearBtn.setBackground(new java.awt.Color(255, 107, 0));
         clearBtn.setFont(new java.awt.Font("Arial", 1, 20)); // NOI18N
-        clearBtn.setForeground(new java.awt.Color(255, 255, 255));
+        clearBtn.setForeground(new java.awt.Color(51, 51, 51));
         clearBtn.setText("CLEAR");
         clearBtn.setBorder(javax.swing.BorderFactory.createCompoundBorder());
         clearBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -122,9 +113,14 @@ public class ForgotPassword extends javax.swing.JFrame {
 
         jButton1.setBackground(new java.awt.Color(255, 107, 0));
         jButton1.setFont(new java.awt.Font("Arial", 1, 20)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setForeground(new java.awt.Color(51, 51, 51));
         jButton1.setText("CONFIRM");
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
 
         newPwCheckbox.setText("See Password");
         newPwCheckbox.addActionListener(new java.awt.event.ActionListener() {
@@ -140,16 +136,12 @@ public class ForgotPassword extends javax.swing.JFrame {
             }
         });
 
-        oldPwCheckbox.setText("See Password");
-        oldPwCheckbox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oldPwCheckboxActionPerformed(evt);
-            }
-        });
-
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/exit.png"))); // NOI18N
-        jLabel1.setText("exit");
+        jLabel1.setText("EXIT");
         jLabel1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+
+        tokenField.setBackground(new java.awt.Color(227, 235, 219));
 
         javax.swing.GroupLayout forgotPwPanelLayout = new javax.swing.GroupLayout(forgotPwPanel);
         forgotPwPanel.setLayout(forgotPwPanelLayout);
@@ -164,15 +156,13 @@ public class ForgotPassword extends javax.swing.JFrame {
                 .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(forgotPwPanelLayout.createSequentialGroup()
                         .addComponent(clearBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 134, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(302, 302, 302))
                     .addGroup(forgotPwPanelLayout.createSequentialGroup()
-                        .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(emailField1, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
-                                .addComponent(oldPwField1))
-                            .addComponent(oldPwCheckbox))
+                        .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(emailField1)
+                            .addComponent(tokenField, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE))
                         .addGap(33, 33, 33)
                         .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(forgotPwPanelLayout.createSequentialGroup()
@@ -189,7 +179,7 @@ public class ForgotPassword extends javax.swing.JFrame {
                                     .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addComponent(confirmPwCheckbox)
                                         .addComponent(confirmPwField1, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                        .addContainerGap(102, Short.MAX_VALUE))))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         forgotPwPanelLayout.setVerticalGroup(
             forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -205,13 +195,11 @@ public class ForgotPassword extends javax.swing.JFrame {
                 .addGap(67, 67, 67)
                 .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(oldPwLabel)
-                    .addComponent(oldPwField1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(confirmPwLabel)
-                    .addComponent(confirmPwField1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(confirmPwField1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tokenField, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(confirmPwCheckbox)
-                    .addComponent(oldPwCheckbox))
+                .addComponent(confirmPwCheckbox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
                 .addGroup(forgotPwPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -224,7 +212,7 @@ public class ForgotPassword extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(forgotPwPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(forgotPwPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -252,15 +240,6 @@ public class ForgotPassword extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_confirmPwCheckboxActionPerformed
 
-    private void oldPwCheckboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_oldPwCheckboxActionPerformed
-        if(oldPwCheckbox.isSelected()){
-            oldPwField1.setEchoChar((char)0);
-        }
-        else{
-            oldPwField1.setEchoChar('*');
-        }
-    }//GEN-LAST:event_oldPwCheckboxActionPerformed
-
     private void emailField1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_emailField1MouseClicked
         emailField1.setText("");
     }//GEN-LAST:event_emailField1MouseClicked
@@ -273,16 +252,41 @@ public class ForgotPassword extends javax.swing.JFrame {
         confirmPwField1.setText("");
     }//GEN-LAST:event_confirmPwField1MouseClicked
 
-    private void oldPwField1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_oldPwField1MouseClicked
-        oldPwField1.setText("");
-    }//GEN-LAST:event_oldPwField1MouseClicked
-
     private void clearBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_clearBtnMouseClicked
         emailField1.setText("");
         newPwField1.setText("");
-        oldPwField1.setText("");
+        tokenField.setText("");
         confirmPwField1.setText("");
     }//GEN-LAST:event_clearBtnMouseClicked
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        MySqlConnection mySqlConnection = new MySqlConnection();
+        Connection con = mySqlConnection.openConnection();
+        if (con != null) {
+            String enteredEmail = emailField1.getText();
+            int recordId = 1;
+            String emailFromRecord = mySqlConnection.getEmailFromRecord(con, recordId);
+            if (emailFromRecord != null && enteredEmail.equals(emailFromRecord)) {
+                
+                // Emails match, proceed with password change
+                
+                String newPassword = newPwField1.getText(); // Assuming you get the new password from the field
+                int rowsUpdated = mySqlConnection.updatePasswordByEmail(con, enteredEmail, newPassword);
+                if (rowsUpdated > 0) {
+                    JOptionPane.showMessageDialog(this, "Password updated successfully.");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Failed to update password.");
+}
+                // Add your password change logic here
+            } else {
+                // Emails don't match
+                JOptionPane.showMessageDialog(this, "Emails do not match or email field empty.");
+            }
+            mySqlConnection.closeConnection(con);
+        } else {
+            JOptionPane.showMessageDialog(this, "Database connection Error.");
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
     
     /**
      * @param args the command line arguments
@@ -332,8 +336,7 @@ public class ForgotPassword extends javax.swing.JFrame {
     private javax.swing.JCheckBox newPwCheckbox;
     private javax.swing.JPasswordField newPwField1;
     private javax.swing.JLabel newPwLabel;
-    private javax.swing.JCheckBox oldPwCheckbox;
-    private javax.swing.JPasswordField oldPwField1;
     private javax.swing.JLabel oldPwLabel;
+    private javax.swing.JTextField tokenField;
     // End of variables declaration//GEN-END:variables
 }
